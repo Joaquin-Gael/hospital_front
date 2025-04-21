bdcd1dee26673e2d597b4fae547ed7619bf31626
Just Fontaine <itsmealeexvii@gmail.com>
2025-04-21 13:54:31 -0300

    Fix: evitar envío de token en el registro de usuarios

--- src/app/interceptor/custom.interceptor.ts
+++ src/app/interceptor/custom.interceptor.ts
@@ -1,13 +1,20 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const customInterceptor: HttpInterceptorFn = (req, next) => {
-  debugger;
   const token = localStorage.getItem('auth_token');
-  const clonedReq = req.clone({
-    setHeaders: {
-      Authorization: `Bearer ${token}`
-    }
-  })
-  
-  return next(clonedReq);
+
+  if (req.url.includes('/users/add')) {
+    return next(req);
+  }
+
+  if (token) {
+    const clonedReq = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(clonedReq);
+  }
+
+  return next(req);
 };
